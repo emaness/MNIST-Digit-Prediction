a0e628f9e1a924ce845b32f736f6586d2199be7c
Everett Maness <emaness@isi.edu>
2020-04-22 02:04:49 -0700

    submission

--- NeuralNetwork3.py
+++ NeuralNetwork3.py
@@ -1,16 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import time
 from math import ceil
-import argparse
 import sys
-from scipy.special import softmax
-
-start_time = time.time()
-
-# dig = load_digits()
-# onehot_target = pd.get_dummies(dig.target)
-# x_train, x_val, y_train, y_val = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
 
 
 def build_target(target: float) -> list:
@@ -21,48 +11,17 @@ def build_target(target: float) -> list:
     return array
 
 
-def load_train_images(path) -> list:
+def load_images(path) -> list:
     images = np.genfromtxt(path, delimiter=",")
     return images
 
 
 def load_train_labels(path) -> list:
     load = np.genfromtxt(path, delimiter=",")
-    # vector = np.vectorize(build_target, otypes=[int])
-    # labels = vector(load)
+
     labels = [build_target(i) for i in load]
-    # print(labels)
+
     return labels
-
-def load_test_images(path) -> list:
-    images = np.genfromtxt(path, delimiter=",")
-    return images
-
-
-def load_test_labels(path = "./test_label.csv") -> list:
-    load = np.genfromtxt(path, delimiter=",")
-    # vector = np.vectorize(build_target, otypes=[int])
-    # labels = vector(load)
-    labels = [build_target(i) for i in load]
-    # print(labels)
-    return labels
-
-# def ReLU(s):
-#     s[s < 0] = 0
-#     return s
-#
-# def binary_choice(i: float):
-#     if i > 0:
-#         return 1
-#     else:
-#         return 0
-#
-# def d_ReLU(s):
-#     derived = np.array(s.shape)
-#     v_func = np.vectorize(binary_choice)
-#     derived = v_func(s)
-#
-#     return derived
 
 
 def sigmoid(matrix):
@@ -142,32 +101,14 @@ class NeuralNetwork:
 
 
     def feed_forward(self):
-
-        # print("images: ", self.x.shape)
-        # print("weights1: ",self.w1.shape)
-
-        # x1 = np.dot(self.input_images, self.weights_layer1) + self.bias_layer1
         self.o_layer1 = sigmoid(np.dot(self.input_images, self.weights_layer1) + self.bias_layer1)
-
-        # self.a1 = ReLU(z1)
-        # x2 = np.dot(self.o_layer1, self.weights_layer2) + self.bias_layer2
         self.o_layer2 = sigmoid(np.dot(self.o_layer1, self.weights_layer2) + self.bias_layer2)
-
-        # self.a2 = ReLU(z2)
-        # x3 = np.dot(self.o_layer2, self.weights_layer3) + self.bias_layer3
         self.o_layer3 = softmax(np.dot(self.o_layer2, self.weights_layer3) + self.bias_layer3)
 
     def back_propagate(self):
-        # loss = error(self.o_layer3, self.input_labels)
-        # print('Error :', loss)
-        # print(self.a3)
-        # print(self.y)
+
         o_layer3_del = cross_entropy(self.o_layer3, self.input_labels)
-
-        # x_layer2_delta = np.dot(o_layer3_delta, self.weights_layer3.T)
         o_layer2_del = np.dot(o_layer3_del, self.weights_layer3.T) * sigmoid_derivative(self.o_layer2)
-
-        # x_layer1_delta = np.dot(o_layer2_delta, self.weights_layer2.T)
         o_layer1_del = np.dot(o_layer2_del, self.weights_layer2.T) * sigmoid_derivative(self.o_layer1)
 
         self.update_weights(o_layer1_del, o_layer2_del, o_layer3_del)
@@ -189,7 +130,6 @@ class NeuralNetwork:
         self.input_images = data
         self.feed_forward()
         return self.o_layer3.argmax(axis=1)
-        #return self.o_layer3.argmax()
 
     def update_batch(self, batch, labels):
         self.input_images = batch
@@ -198,22 +138,7 @@ class NeuralNetwork:
     def adjust_lr(self, lr):
         self.lr = lr
 
-def output_plots(accuracies, epochs):
-    #learning_rates,
-    # print(accuracies)
-    plt.figure(1)
-    plt.plot(epochs, accuracies)
-    plt.ylabel("Accuracy")
-    plt.xlabel("Epoch")
 
-    # plt.figure(2)
-    # plt.plot(epochs, learning_rates)
-    # plt.ylabel("LR")
-    # plt.xlabel("Epoch")
-    plt.show()
-
-
-# def train(images, labels, test_images, test_labels):
 def train(images, labels, test_images):
 
     batch_size = 10
@@ -224,10 +149,6 @@ def train(images, labels, test_images):
 
     batches = int(ceil(num_examples / batch_size))
 
-    # accuracies = []
-    # epoch_list = []
-    # learning_rates = []
-
     model = NeuralNetwork(images[0:batch_size] / 255.0, labels[0:batch_size])
 
     lr_sched = LrScheduler(.5, .005)
@@ -236,7 +157,7 @@ def train(images, labels, test_images):
 
         for i in range(batches):
 
-            #
+            # choose end_index with variable-sized dataset in mind
             start_index = i * batch_size
             end_index = min(start_index + batch_size, num_examples)
 
@@ -253,28 +174,8 @@ def train(images, labels, test_images):
                 adjustments += 1
                 examples_trained = 0
 
-
-        # accuracies.append(calculate_accuracy(np.array(test_images) / 255.0, np.array(test_labels), model))
-        # epoch_list.append(x)
-        # learning_rates.append(model.lr)
-
-    # print("Test accuracy : ", calculate_accuracy(np.array(test_images) / 255.0, np.array(test_labels), model))
     results = model.predict(test_images / 255.0)
     output_prediction(results)
-    # print("time elapsed: {:.2f}s".format(time.time() - start_time))
-
-    # output_plots(accuracies, epoch_list)
-
-    return model
-
-
-# def calculate_accuracy(x, y, model):
-#     acc = 0
-#     for xx, yy in zip(x, y):
-#         s = model.predict(xx)
-#         if s == np.argmax(yy):
-#             acc += 1
-#     return acc / len(x) * 100
 
 
 def output_prediction(results):
@@ -300,16 +201,8 @@ if __name__ == "__main__":
         test_images_path = sys.argv[3]
 
     all_labels = load_train_labels(train_labels_path)
-    all_images = load_train_images(train_images_path)
+    all_images = load_images(train_images_path)
 
-    test_labels = load_test_labels()
-    test_images = load_test_images(test_images_path)
-
-    # t_images = test_images[0:10000]
-    # t_labels = test_labels[0:10000]
-    #
-    # images = all_images[0:10000]
-    # labels = all_labels[0:10000]
+    test_images = load_images(test_images_path)
 
     train(np.array(all_images), np.array(all_labels), np.array(test_images))
-    # train(np.array(all_images), np.array(all_labels), np.array(test_images), np.array(test_labels))
